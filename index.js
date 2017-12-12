8b4bd7d87943d8a5d3a5dddcda12414b1423177a
abdullah-pk <abdullah@primekeeper.my>
2017-12-12 23:07:48 +0800

    final version

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const claypot = (from, to) => {
-    console.log("Finish me!")
+
+	for( let i = from; i<to; i++){
+	
+	let f = {from %5}
+	let t = {to %3}
+	let t1 = {to %5}
+
+	if (from === 0){
+		console.log("Clay")
+	}else if(t === 0 && t1 ===0){
+		console.log("pot")
+	}else{
+		console.log(i)
+	}
+	}
+	
+
 }
+
+ReactDOM.render(claypot, document.getElementById('app'));
